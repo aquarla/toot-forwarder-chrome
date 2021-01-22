a3087f339626b81a8030ec5d00c9bc47ea68f8dd
aquarla <satosamu@gmail.com>
2021-01-22 16:20:10 +0900

    Testing with verify_credentials API.

--- src/options.ts
+++ src/options.ts
@@ -1,6 +1,8 @@
 import 'materialize-css/dist/js/materialize.min.js'
 import 'materialize-css/dist/css/materialize.min.css'
 
+import axios from 'axios'
+
 interface StorageData {
   name: string,
   domain: string,
@@ -10,15 +12,15 @@ interface StorageData {
 window.onload = () => {
   ['name', 'domain', 'access_token'].forEach(item => {
     chrome.storage.local.get(item, (value) => {
-      const element : HTMLInputElement = <HTMLInputElement>document.getElementById(item)
+      const element: HTMLInputElement = <HTMLInputElement>document.getElementById(item)
       if (value[item]) {
         element.value = value[item];
       }
-    })
+    });
   });
-  
+
   document.getElementById('save').onclick = () => {
-    const values : StorageData = {
+    const values: StorageData = {
       name: (<HTMLInputElement>document.getElementById('name')).value,
       domain: (<HTMLInputElement>document.getElementById('domain')).value,
       access_token: (<HTMLInputElement>document.getElementById('access_token')).value
@@ -26,5 +28,23 @@ window.onload = () => {
     chrome.storage.local.set(values, () => {
       alert('データを保存しました');
     });
+  }
+
+  document.getElementById('verify-credentials').onclick = () => {
+    const domain = (<HTMLInputElement>document.getElementById('domain')).value
+    const access_token = (<HTMLInputElement>document.getElementById('access_token')).value
+    const instance = axios.create({
+      baseURL: `https://${domain}/`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+    instance.get(`api/v1/accounts/verify_credentials`)
+      .then(() => {
+        alert('接続のテストに成功しました');
+      }).catch(() => {
+        alert('接続のテストに失敗しました。設定内容を確認してください');
+      });
   }
 }
